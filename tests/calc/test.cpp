f422bba6b86587dd4036c3af8dfb0b42474a009b
SteelPh0enix <wojciech_olech@hotmail.com>
2024-03-15 16:04:37 +0100

    Calc test

--- tests/calc/test.cpp
+++ tests/calc/test.cpp
@@ -1,10 +1,23 @@
 #include <CppUTest/CommandLineTestRunner.h>
 #include <CppUTest/TestHarness.h>
 
-TEST_GROUP(FirstTestGroup){};
+#include <calc/calc.hpp>
 
-TEST(FirstTestGroup, FirstTest) {
-    FAIL("Fail me!");
+TEST_GROUP(TemperatureCalcTests){};
+
+TEST(TemperatureCalcTests, convertsCelciusToFahrenheit) {
+    auto const givenCelcius = 37.7778;
+    auto const expectedFahrenheit = 100.0;
+    auto const gotFahrenheit = celcius_to_fahrenheit(givenCelcius);
+    DOUBLES_EQUAL(expectedFahrenheit, gotFahrenheit, 0.001);
+}
+
+
+TEST(TemperatureCalcTests, convertsFahrenheitToCelcius) {
+    auto const givenFahrenheit = 212;
+    auto const expectedCelcius = 100.0;
+    auto const gotCelcius = fahrenheit_to_celcius(givenFahrenheit);
+    DOUBLES_EQUAL(expectedCelcius, gotCelcius, 0.001);
 }
 
 int main(int ac, char** av) {
